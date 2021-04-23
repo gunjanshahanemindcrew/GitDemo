9fae3679bc48dbe187a3ed50a9b514dd9b3470e8
Vivek Virde <vivek.virde@nerdpeople.com>
2021-04-23 17:17:56 +0530

    Added add.js with some functions

---
+++ add.js
@@ -0,0 +1,8 @@
+// Simple Addition Function
+const add = (x, y, z) => {
+  return parseInt(x) + parseInt(y) + parseInt(z);
+};
+
+let a = [2, 3, 5];
+console.log("Added", add(...a));
+console.log("Hello World on Initial File");
